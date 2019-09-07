19005e470747519bd8e84d44c48faa3749084b60
delola\delola lokal <aleksandra.man@gmail.com>
2019-09-07 21:58:03 +0200

    lotto simulator in progess

--- src/lotto_simulator/lottoSimulator.java
+++ src/lotto_simulator/lottoSimulator.java
@@ -10,6 +10,7 @@ public class lottoSimulator {
 
     public static void main (String [] args) {
         lottoGenerator();
+        randomNumbers();
     }
 
     public static void lottoGenerator() {
@@ -23,45 +24,42 @@ public class lottoSimulator {
         int userNr5 = scanner.nextInt();
         int userNr6 = scanner.nextInt();
 
-        int [] userArray = new int [6];
-        userArray = new int [] {userNr1, userNr2, userNr3, userNr4, userNr5, userNr6};
+        int[] userArray = new int[6];
+        userArray = new int[]{userNr1, userNr2, userNr3, userNr4, userNr5, userNr6};
         sort(userArray);
         System.out.println(Arrays.toString(userArray));
+    }
 
-        int numberCount = 0;
+    public static void randomNumbers() {
         Random r = new Random();
         System.out.println("Komputer losuje 6 liczb od 0 do 49:");
-        int r1;
 
-        r1 = 1 + r.nextInt(50);
+        int[] allNumbers = new int[49];
+        int[] randomNumbers = new int[6];
 
-        int [] allNumbers = new int[49];
-        int [] randomNumbers = new int[6];
-
-        for(int i = 0; i < allNumbers.length; i++) {
+        for (int i = 0; i < allNumbers.length; i++) { //wypełniam tablicę liczbami od 1 do 49
             allNumbers[i] = 1 + i;
         }
-        System.out.println(Arrays.toString(allNumbers));
 
-        int i = 0;
-
-        for (int j = 0; j < 6; j++){
+        int i = 0; //indeks w małej tablicy (tej, którą będę zapełniać 6 kolejnymi liczbami)
+        for (int j = 0; j < 6; j++) { //losuję 6 liczb z wcześniej tworzonej tablicy +
             i = r.nextInt(49 - j);
             randomNumbers[j] = allNumbers[i];
-            allNumbers[i] = allNumbers[allNumbers.length - j - 1];
-        }
+            allNumbers[i] = allNumbers[allNumbers.length - j - 1];// + przesuwam wylosowaną liczbę na ostatni indeks z dużej tablicy
+        }                                                          // i dalej losuję już w mniejszej tablicy (bo na ostatni indeks wrzciłam przed chwilą wylosowaną liczbę)
+        //dzięki temu am pewność, że te liczby się nigdy nie powtórzą
 
         sort(randomNumbers);
-
         System.out.println(Arrays.toString(randomNumbers));
-
-
-
-
-
-
-
     }
+
+
+
+
+
+
+
+
 
 
 }
